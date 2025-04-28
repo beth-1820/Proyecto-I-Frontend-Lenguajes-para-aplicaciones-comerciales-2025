2196dfcec0241d4bdb9c1383e3543f5197174f08
beth-1820 <beth_1820@outlook.com>
2025-04-27 20:24:42 -0600

    mis cambios

--- Proyecto-Lenguajes-Frontend/src/app/app.routes.ts
+++ Proyecto-Lenguajes-Frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ClienteListComponent } from './features/clientes/cliente-list/cliente-list.component';
+import { ClienteFormComponent } from './features/clientes/cliente-form/cliente-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  /* listado principal */
+  { path: 'clientes',        component: ClienteListComponent },
+
+  /* formulario para crear nuevo cliente */
+  { path: 'clientes/nuevo',  component: ClienteFormComponent },
+
+  /* HOME → redirige a la lista */
+  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
+
+  /* Wildcard (404) */
+  { path: '**', redirectTo: 'clientes' }
+];
