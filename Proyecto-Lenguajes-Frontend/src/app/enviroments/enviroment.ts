c60d6e8b5100444214bf4a0818438340239c23f5
beth-1820 <beth_1820@outlook.com>
2025-06-01 19:53:23 -0600

    Environment listo no tocar mas

--- Proyecto-Lenguajes-Frontend/src/app/enviroments/enviroment.ts
+++ Proyecto-Lenguajes-Frontend/src/app/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false, //Esta la agregue, revisen, si falla la eliminan
-    API_URL: 'http://localhost:8080/vitalitycenter/api/'
+    API_URL: 'http://localhost:8080/vitalitycenter/'
   };
   
